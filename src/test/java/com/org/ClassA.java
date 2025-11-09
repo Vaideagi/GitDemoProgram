ea3ed67db785cf517469a9465426954fc3818ae0
vaideagi <pvaideagi@gmail.com>
2025-11-09 16:05:38 +0530

    new project push

---
+++ src/test/java/com/org/ClassA.java
@@ -0,0 +1,13 @@
+package com.org;
+
+public class ClassA {
+ public void a1() {
+	 System.out.println("hi");
+ }
+ public void a2() {
+	 System.out.println("welcome");
+ }
+ public void a3() {
+	 System.out.println("hello world");
+ }
+}
